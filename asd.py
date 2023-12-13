103bcc55ff447d633e72cfd09935b54e2495ccd4
Fazekas06 <fazekasmarkkrisztian@turr.hu>
2023-12-13 10:25:00 +0100

    terulet eredmeny

--- asd.py
+++ asd.py
@@ -1,5 +1,6 @@
 a=int(input("ad meg az a oldalt"))
 b=int(input("ad meg a b oldalt"))
+eredmeny=a+b
 x=(input("terület, vagy kerületet kell számítani?"))
 if x==("terület"):
-    print
+    print("a terület:",)
